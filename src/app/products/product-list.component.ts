07352b045d8f291d059427cda09adb9dd4ba1c71
dev-marko <markospasenovski00@gmail.com>
2023-02-08 17:06:56 +0100

    Completed modules: - More on Components - Building Nested Components - Services and Dependency Injection - Retrieving Data Using HTTP - Navigation and Routing Basics

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,39 +1,65 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import {  Subscription } from "rxjs";
+
+import { ProductService } from "./product.service";
+
+import { IProduct } from "./product";
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-          },
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-    ];
+    errorMessage: string = '';
+    sub!: Subscription
+    // We don't want to filter our original products array
+    // because that way we lose our data
+    filteredProducts: IProduct[] = [];
+    products: IProduct[] = [];
+    
+    constructor(private productService: ProductService) {}
+
+    private _listFilter: string = '';
+
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.performFilter(value);
+    }
+
+    ngOnInit(): void {
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err,
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLowerCase();
+        // If the filter string is empty, it returns all products
+        return this.products.filter((item: IProduct) => item.productName.toLowerCase().includes(filterBy));
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
+
 }
